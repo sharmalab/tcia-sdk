5e8131237ef90179c5e34ee198df4c7e1c0a422b
Pradeeban <kk.pradeeban@gmail.com>
2018-09-06 11:35:39 -0400

    Organize the imports of the util class

--- tcia-rest-client-java/src/main/java/edu/emory/cci/tcia/client/util/TCIAClientUtil.java
+++ tcia-rest-client-java/src/main/java/edu/emory/cci/tcia/client/util/TCIAClientUtil.java
@@ -1,20 +1,5 @@
 package edu.emory.cci.tcia.client.util;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import edu.emory.cci.tcia.client.core.OutputFormat;
-import edu.emory.cci.tcia.client.exceptions.TCIAClientException;
-import edu.emory.cci.tcia.client.conf.TCIAConf;
-import edu.emory.cci.tcia.client.conf.TCIAConstants;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -23,7 +8,23 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Base64;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.client.HttpClient;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import edu.emory.cci.tcia.client.core.OutputFormat;
+import edu.emory.cci.tcia.client.exceptions.TCIAClientException;
+import edu.emory.cci.tcia.client.conf.TCIAConf;
+import edu.emory.cci.tcia.client.conf.TCIAConstants;
 
 
 /**
