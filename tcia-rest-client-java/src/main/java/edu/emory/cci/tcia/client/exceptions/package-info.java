4ad0a88d5b8aea80268d1e2a9dd5aeb05af7081a
Pradeeban <kk.pradeeban@gmail.com>
2018-09-06 11:29:57 -0400

    Exception and Impl packages

---
+++ tcia-rest-client-java/src/main/java/edu/emory/cci/tcia/client/exceptions/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The implementations of TCIA Client Exceptions
+ */
+package edu.emory.cci.tcia.client.exceptions;
---
+++ tcia-rest-client-java/src/main/java/edu/emory/cci/tcia/client/impl/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The implementation classes of TCIA Client.
+ */
+package edu.emory.cci.tcia.client.impl;
